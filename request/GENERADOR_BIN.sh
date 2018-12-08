86df42bd3be81a28dd08aef1fdfca6243358fa15
Jars <ar851304@gmail.com>
2018-12-08 01:14:59 -0300

    Update Script

--- request/GENERADOR_BIN.sh
+++ request/GENERADOR_BIN.sh
@@ -2,10 +2,6 @@
 # Bin _ Gen #OFC
 link_bin="https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/ADM-ULTIMATE-NEW-FREE/master/Install/generadorcc.py"
 [[ ! -e /usr/bin/generadorcc.py ]] && wget -O /usr/bin/generadorcc.py ${link_bin} > /dev/null 2>&1 && chmod +x /usr/bin/generadorcc.py
-SCPdir="/etc/newadm" && [[ ! -d ${SCPdir} ]] && exit 1
-SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
-SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
-SCPidioma="${SCPdir}/idioma" && [[ ! -e ${SCPidioma} ]] && touch ${SCPidioma}
 msg -ama "$(fun_trans "GERADOR DE BINS OFICIAL")"
 msg -bar
 msg -ne "$(fun_trans "Digite a bin"): " && read UsrBin
