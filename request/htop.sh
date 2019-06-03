4e8713b742c39417816bb31e4b7fcddf05260d0a
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2019-06-02 20:51:56 -0400

    Update Script

---
+++ request/htop.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;32m" [3]="\033[1;36m" [4]="\033[1;31m" [5]="\033[1;33m" )
+barra="\033[0m\e[34m======================================================\033[1;37m"
+SCPdir="/etc/newadm" && [[ ! -d ${SCPdir} ]] && exit
+SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
+SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
+SCPidioma="${SCPdir}/idioma" && [[ ! -e ${SCPidioma} ]] && touch ${SCPidioma}
+
+fun_bar () {
+comando="$1"
+ _=$(
+$comando > /dev/null 2>&1
+) & > /dev/null
+pid=$!
+while [[ -d /proc/$pid ]]; do
+echo -ne " \033[1;33m["
+   for((i=0; i<10; i++)); do
+   echo -ne "\033[1;31m##"
+   sleep 0.2
+   done
+echo -ne "\033[1;33m]"
+sleep 1s
+echo
+tput cuu1 && tput dl1
+done
+echo -e " \033[1;33m[\033[1;31m####################\033[1;33m] - \033[1;32m100%\033[0m"
+sleep 1s
+}
+
+echo -e "${cor[5]} $(fun_trans "HTOP PROCESOS DE SISTEMA")"
+echo -e "${cor[5]} $(fun_trans "Con HTOP puedes verificar todos los procesos del sistema")"
+echo -e "$barra"
+echo -e "${cor[0]} $(fun_trans "Iniciando Aguarde")"
+echo -e " "
+fun_bar "apt-get install htop -y"
+echo -e " "
+sleep 1s
+echo -e "${cor[0]}PARA SALIR DEL PANEL PRESIONE ${cor[5]}CTLR+C \e[0m"
+sleep 5s
+echo -e "$barra"
+
+htop
