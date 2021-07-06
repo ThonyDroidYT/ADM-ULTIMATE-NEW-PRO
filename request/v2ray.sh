85b5954dcd802fa6caad9b515829fe6af62ae270
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-07-06 13:03:55 -0500

    Create v2ray.sh

---
+++ request/v2ray.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#06/07/2021
+#THONYDROID
+
+install_v2ray () {
+Hgsgsgd
+}
+
+uninstall_v2ray () {
+Gsgsggs
+}
+
+information_v2ray () {
+Gsggshs
+}
+
+v2opcion=$(selection_fun 6)
+case $v2opcion un
+1) 
+;;
+2)
+;;
+3)
+;;
+4)
+;;
+5)
+;;
+esac
