cb214ecf74cee3e95750f9355404acdee0b169ac
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-07-06 13:41:16 -0500

    Update v2ray.sh

--- request/v2ray.sh
+++ request/v2ray.sh
@@ -8,21 +8,28 @@ SCPinst="/etc/ger-inst" && [[ ! -d ${SCPfrm} ]] && exit 1 #mkdir ${SCPfrm}
 
 install_v2ray () {
 source <(curl -sL https://multi.netlify.app/v2ray.sh)
+
+${SCPinst}/v2ray.sh
 }
 
 uninstall_v2ray () {
 source <(curl -sL https://multi.netlify.app/v2ray.sh) --remove
+
+${SCPinst}/v2ray.sh
 }
 
 information_v2ray () {
 v2ray info
+
+${SCPinst}/v2ray.sh
 }
+
 msg -ama "$(fun_trans "MENU V2RAY") $(msg -verd "[ADM-ULTIMATE-NEW-PRO]")"
 menu_func "" ""
 msg -verd "[0] $(msg -verm2 ">") $(msg -bra "$(fun_trans "REGRESAR")")"
 msg -bar
 v2opcion=$(selection_fun 6)
-case $v2opcion un
+case $v2opcion in
 1) 
 ;;
 2)
